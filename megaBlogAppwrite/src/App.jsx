0e80b5e461578f5343e9bbb05ca4241b9b78dae8
sudha <sudhapalyal81@gmail.com>
2026-08-23 16:05:51 +0530

    Mega Blog Appwrite Project

--- megaBlogAppwrite/src/App.jsx
+++ megaBlogAppwrite/src/App.jsx
@@ -1,12 +1,44 @@
+import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import {Footer} from "./components";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+import "./App.css"
+
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+      authService.getCurrentUser()
+      .then((userData) => {
+         if(userData){
+           dispatch(login({userData}))
+         }else{
+          dispatch(logout())
+         }
+      })
+      .finally(() => setLoading(false))
+      
+  },[])
+  
   return (
-    <>
-      <h1>A blog app using appwrite</h1>
-    </>
-  )
+  !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header />
+      <main>
+       TODO : <Outlet />
+      </main>
+      <Footer />
+    </div>
+    </div>
+  ) : null
+)
+  
 }
 
 export default App
---
+++ megaBlogAppwrite/src/appwrite/config.js
@@ -0,0 +1,149 @@
+import conf from "../conf/conf";
+import { Client, ID, TablesDB, Storage, Query } from "appwrite";
+
+export class Service{
+
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appWriteUrl)
+        .setProject(conf.appWriteProjectId);
+        this.databases = new TablesDB(this.client);  //DB operations ke liye
+        this.bucket = new Storage(this.client);      //File operations ke liye
+    }
+
+    //Post services
+     
+    //create post service
+    async createPost({title, slug, content, featuredImage,status,userId}){
+        try{
+            return await this.databases.createRow(
+               {
+            databaseId: conf.appWriteDatabaseId,
+            tableId: conf.appWriteCollectionId,
+            rowId: slug,
+            data: {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId,
+            }
+        }
+            )
+        }catch(error){
+          console.log("Appwrite service :: createPost :: error", error);
+        }
+    }
+
+    //update post service
+    async updatePost(slug , {title, content, featuredImage, status}){
+        try{
+          return await this.databases.updateRow({
+             databaseId: conf.appWriteDatabaseId,
+             tableId: conf.appWriteCollectionId, 
+             rowId: slug,
+             data: {
+                    title,
+                    content, 
+                    featuredImage, 
+                    status,
+
+                }
+         })
+        }catch(error){
+              console.log("Appwrite service :: updatePost :: error", error);
+        
+        }
+    }
+
+    //delete post service
+    async deletePost(slug){
+        try{
+            await this.databases.deleteRow({
+               databaseId: conf.appWriteDatabaseId,
+               tableId: conf.appWriteCollectionId, 
+               rowId: slug})
+            return true;
+
+        }catch(error){
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
+        }
+    }
+
+    //get post service
+    async getPost(slug){
+        try{
+            return await this.databases.getRow({
+               databaseId: conf.appWriteDatabaseId,
+               tableId: conf.appWriteCollectionId, 
+               rowId: slug})
+
+        }catch(error){
+            console.log("Appwrite service :: getPost :: error", error);
+            return false;
+        }
+    }
+
+    // get posts service
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try{
+            return await this.databases.listRows({
+                 databaseId: conf.appWriteDatabaseId,
+                 tableId: conf.appWriteCollectionId,
+                 queries: queries
+        })
+        }catch(error){
+             console.log("Appwrite service :: getPosts :: error", error);
+             return false;
+        }
+    }
+
+
+    //File services 
+
+    //file upload service
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        }catch(error){
+            console.log("Appwrite service :: uploadFile ::error", error);
+            return false;
+        }
+    }
+
+    //delete file service
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+             return true;
+        }catch(error){
+            console.log("Appwrite service :: deleteFile :: error", error);
+            return false;
+        }
+    }
+
+    //preview file service
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appWriteBucketId,
+            fileId
+        )
+
+    }
+}
+
+const service = new Service();                         
+                                                            
+export default service; 
