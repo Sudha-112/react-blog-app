fe9a978e86ee6811c3bb59bdc1d2cdf4c4cfbe4a
sudha <sudhapalyal81@gmail.com>
2026-08-27 14:42:11 +0530

    css correction

--- megaBlogAppwrite/src/components/Header/Header.jsx
+++ megaBlogAppwrite/src/components/Header/Header.jsx
@@ -42,19 +42,19 @@ function Header() {
   return (
     <header className="py-3 shadow bg-gray-500">
       <Container>
-        <nav className="flex">
+        <nav className="flex flex-wrap items-center ">
           <div className="mr-4">
               <Link to='/'>
               <Logo width='70px' />
               </Link>
           </div>
-          <ul className="flex ml-auto">
+          <ul className="flex flex-wrap ml-auto gap-2">
              {navItems.map((item) => 
             item.active ? (
               <li key={item.name}>
                 <button onClick={() => navigate(item.slug)}
-                  className='inline-block px-6 py-2
-                   duration-200 hover:bg-blue-100 rounded-full'>
+                  className='inline-block px-3 sm:px-6 py-2 duration-200
+                   hover:bg-blue-100 rounded-full text-sm sm:text-base'>
                   {item.name}
                 </button>
               </li>
